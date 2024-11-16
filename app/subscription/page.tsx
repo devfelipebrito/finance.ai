b19f467f796e416de49fb6effb56dc124743fdb7
Felipe Brito <felipebrito.dev@gmail.com>
2024-11-16 00:24:24 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
-const SubscritonPage = () => {
+const SubscritonPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return <Navbar />;
 };
 
